496ef16e0e5f0b813baa66a558df3e278d9a03e2
mike <apolloproductionsdj@gmail.com>
2021-06-15 23:42:15 -0700

    updated next.js updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 
 export default function Home() {
@@ -12,10 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
 
-      {/* Nav */}
+      <Nav />
 
       {/* Results */}
     </div>
